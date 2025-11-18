7f77a390ada04ed9eba12ce8d2b5b4b8fca0b4d9
Power <ismailovgamzatmaga@gmail.com>
2025-11-18 16:20:40 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1;
+
+namespace Vosmerka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            // Исправленный вариант для нестатического MainFrame:
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.MainFrame.Navigate(this);
+            }
+        }
+        private void funcButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ButtonProduct_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ WpfApp1/ProductPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vosmerka
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void FilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
